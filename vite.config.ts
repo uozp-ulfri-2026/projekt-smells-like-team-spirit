977ab23a5825c811e154930d45539c1167d44bb0
Luka Pršina <prsinaluka@gmail.com>
2026-05-09 14:06:44 +0200

    stack

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	plugins: [
-		tailwindcss(),
-		tsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tanstackStart(),
-		viteReact(),
-	],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
